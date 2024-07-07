da3aed5eb93f058ef87a87edb574c49151cb9ac3
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-07-07 16:10:31 -0300

    feat: Insere novas funcionalidades na tela de login
    - Formulário tem um botão de reset;
    - E agora faz request para o servidor e armazena o token nos cookies

--- view/view/src/app/app.component.ts
+++ view/view/src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from './components/button/button.component';
-import { ImageContainerComponent } from './components/image-container/image-container.component';
-import { InputComponent } from './components/input/input.component';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RouterOutlet } from '@angular/router';
+import { ResponseToken } from './@types/response-token';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
+import { AuthenticationService } from './service/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +21,9 @@ import { MatIcon } from '@angular/material/icon';
     RouterOutlet,
     ButtonComponent,
     InputComponent,
-    ImageContainerComponent,
     ReactiveFormsModule,
-    MatIcon
+    MatIcon,
+    NgIf,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -23,7 +31,10 @@ import { MatIcon } from '@angular/material/icon';
 export class AppComponent {
   form: FormGroup;
 
-  constructor() {
+  constructor(
+    private authService: AuthenticationService,
+    private snackbar: MatSnackBar
+  ) {
     this.form = new FormGroup({
       username: new FormControl(''),
       password: new FormControl(''),
@@ -35,6 +46,35 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log(this.form.value);
+    const username = this.form.get('username')?.value;
+    const password = this.form.get('password')?.value;
+
+    this.authService.loginUser(username, password).subscribe({
+      next: (response: ResponseToken | any) => {
+        document.cookie = `token=${response.token}`;
+
+        this.snackbar.open('Login efetuado com sucesso!', 'Fechar', {
+          duration: 5000,
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
+        });
+      },
+      error: (error: any) => {
+        this.snackbar.open(
+          'Falha ao fazer login. Verifique suas credenciais.', 'Fechar',
+          {
+            duration: 5000,
+            verticalPosition: 'top',
+            horizontalPosition: 'center',
+          }
+        );
+      },
+    });
+
+    this.form.reset();
+  }
+
+  onClean() {
+    this.form.reset();
   }
 }
