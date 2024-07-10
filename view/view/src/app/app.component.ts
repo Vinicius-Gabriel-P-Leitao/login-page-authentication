3d28e158121a9258308db1b6fe7966969ce1492b
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-07-10 02:33:12 -0300

    feat: Implementa rotas no projeto

--- view/view/src/app/app.component.ts
+++ view/view/src/app/app.component.ts
@@ -1,80 +1,13 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  AbstractControl,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { MatIcon } from '@angular/material/icon';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { RouterOutlet } from '@angular/router';
-import { ResponseToken } from './@types/response-token';
-import { ButtonComponent } from './components/button/button.component';
-import { InputComponent } from './components/input/input.component';
-import { AuthenticationService } from './service/authentication.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    ButtonComponent,
-    InputComponent,
-    ReactiveFormsModule,
-    MatIcon,
-    NgIf,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  form: FormGroup;
-
-  constructor(
-    private authService: AuthenticationService,
-    private snackbar: MatSnackBar
-  ) {
-    this.form = new FormGroup({
-      username: new FormControl(''),
-      password: new FormControl(''),
-    });
-  }
-
-  getFormControl(control: AbstractControl | null): FormControl {
-    return control as FormControl;
-  }
-
-  onSubmit() {
-    const username = this.form.get('username')?.value;
-    const password = this.form.get('password')?.value;
-
-    this.authService.loginUser(username, password).subscribe({
-      next: (response: ResponseToken | any) => {
-        document.cookie = `token=${response.token}`;
-
-        this.snackbar.open('Login efetuado com sucesso!', 'Fechar', {
-          duration: 5000,
-          verticalPosition: 'top',
-          horizontalPosition: 'center',
-        });
-      },
-      error: (error: any) => {
-        this.snackbar.open(
-          'Falha ao fazer login. Verifique suas credenciais.', 'Fechar',
-          {
-            duration: 5000,
-            verticalPosition: 'top',
-            horizontalPosition: 'center',
-          }
-        );
-      },
-    });
-
-    this.form.reset();
-  }
-
-  onClean() {
-    this.form.reset();
-  }
+  title = 'view';
 }
--- view/view/src/app/app.routes.ts
+++ view/view/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redireciona quando a url for vazia para o login e depois associa o login ao componente
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  // Associa o register ao componente
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+];
