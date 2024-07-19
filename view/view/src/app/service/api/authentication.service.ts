5f8e27b0fe792a1799552bdda40aff82cd35f72d
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-07-19 01:21:04 -0300

    feat: Adiciona a função de registrar um novo user

--- view/view/src/app/service/api/authentication.service.ts
+++ view/view/src/app/service/api/authentication.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
+import { CookieService } from '../web/cookie.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private cookieService: CookieService) {
     this.http = http;
   }
 
+  /**
+   * Método é responsável por logar um usuário no sistema
+   * @param username Nome de usuário
+   * @param password Senha do usuário
+   * @returns Retorna o token gerado
+   */
   private login(username: string, password: string) {
     const headers = new HttpHeaders({
       username: username,
@@ -25,10 +32,16 @@ export class AuthenticationService {
     return this.login(username, password);
   }
 
-
-
+  /**
+   * Método é responsável por registrar um usuário no sistema
+   * @param username Nome de usuário
+   * @param password Senha do usuário
+   * @param role Role do usuário
+   * @returns Retorna o token gerado
+   */
   private register(username: string, password: string, role: string) {
     const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.cookieService.getCookies('token'),
       username: username,
       password: password,
       role: role,
@@ -37,5 +50,9 @@ export class AuthenticationService {
     return this.http.post(environment.api.url + '/register', null, {
       headers: headers,
     });
+  }
+
+  registerUser(username: string, password: string, role: string) {
+    return this.register(username, password, role);
   }
 }
